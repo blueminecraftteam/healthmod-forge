5a189ec953c05da5c9d77cbed039bb009041d933
AG6 <bean@bean.bean>
2020-11-28 12:06:38 +0000

    license headers :rolling_eyes:

--- src/main/java/io/github/blueminecraftteam/healthmod/core/config/CommonConfig.java
+++ src/main/java/io/github/blueminecraftteam/healthmod/core/config/CommonConfig.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2020 Blue Minecraft Team.
+ *
+ * This file is part of HealthMod.
+ *
+ * HealthMod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HealthMod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HealthMod.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.blueminecraftteam.healthmod.core.config;
 
 import io.github.blueminecraftteam.healthmod.HealthMod;
@@ -5,13 +24,14 @@ import net.minecraftforge.common.ForgeConfigSpec;
 
 public class CommonConfig {
     public final ForgeConfigSpec.BooleanValue canSleepWithoutMask;
-    public CommonConfig(ForgeConfigSpec.Builder builder){
-           builder.comment("This is the configuration for HealthMod.");
-           builder.push("Misc");
-           canSleepWithoutMask =
-                   builder.comment("Whether you can sleep without a mask.")
-                   .translation(HealthMod.MOD_ID + ".config." + "canSleepWithoutMask")
-                   .define("canSleepWithoutMask", true);
-           builder.pop();
+
+    public CommonConfig(ForgeConfigSpec.Builder builder) {
+        builder.comment("This is the configuration for HealthMod.");
+        builder.push("Misc");
+        canSleepWithoutMask =
+                builder.comment("Whether you can sleep without a mask.")
+                        .translation(HealthMod.MOD_ID + ".config." + "canSleepWithoutMask")
+                        .define("canSleepWithoutMask", true);
+        builder.pop();
     }
 }
