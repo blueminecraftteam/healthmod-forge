0d05550626ebe7e620dfd56bca5639a3f0d45a97
AG6 <bean@bean.bean>
2020-12-03 16:12:04 +0000

    test to see if the commit got reverted

--- src/main/java/io/github/blueminecraftteam/healthmod/common/items/AntiBioticsItem.java
+++ src/main/java/io/github/blueminecraftteam/healthmod/common/items/AntiBioticsItem.java
@@ -19,10 +19,20 @@
 
 package io.github.blueminecraftteam.healthmod.common.items;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
 
 public class AntiBioticsItem extends Item {
     public AntiBioticsItem(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 }
