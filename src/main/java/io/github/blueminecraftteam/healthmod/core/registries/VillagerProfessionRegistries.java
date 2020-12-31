51f4af45fc381f89defdb18e872f2643c652e01f
xf8b <unknownperson23fs6zc@protonmail.com>
2020-12-31 08:22:21 -0800

    intellijank irrr

--- src/main/java/io/github/blueminecraftteam/healthmod/core/registries/VillagerProfessionRegistries.java
+++ src/main/java/io/github/blueminecraftteam/healthmod/core/registries/VillagerProfessionRegistries.java
@@ -38,6 +38,18 @@ public class VillagerProfessionRegistries {
     public static final DeferredRegister<VillagerProfession> PROFESSIONS = DeferredRegister.create(ForgeRegistries.PROFESSIONS, HealthMod.MOD_ID);
     public static final DeferredRegister<PointOfInterestType> POI_TYPES = DeferredRegister.create(ForgeRegistries.POI_TYPES, HealthMod.MOD_ID);
 
+    private static final Set<BlockState> DOCTOR_POINT_OF_INTEREST_STATES = new HashSet<>();
+
+    public static final RegistryObject<PointOfInterestType> DOCTOR_POINT_OF_INTEREST = POI_TYPES.register(
+            "doctor_point_of_interest",
+            () -> new PointOfInterestType(
+                    "doctor_point_of_interest",
+                    ImmutableSet.copyOf(DOCTOR_POINT_OF_INTEREST_STATES),
+                    1,
+                    1
+            )
+    );
+
     public static final RegistryObject<VillagerProfession> DOCTOR = PROFESSIONS.register(
             "doctor",
             () -> new VillagerProfession(
@@ -46,16 +58,6 @@ public class VillagerProfessionRegistries {
                     ImmutableSet.of(ItemRegistries.FIRST_AID_KIT.get()),
                     ImmutableSet.of(BlockRegistries.BAND_AID_BOX.get()),
                     SoundEvents.BLOCK_BREWING_STAND_BREW
-            )
-    );
-    private static final Set<BlockState> DOCTOR_POINT_OF_INTEREST_STATES = new HashSet<>();
-    public static final RegistryObject<PointOfInterestType> DOCTOR_POINT_OF_INTEREST = POI_TYPES.register(
-            "doctor_point_of_interest",
-            () -> new PointOfInterestType(
-                    "doctor_point_of_interest",
-                    ImmutableSet.copyOf(DOCTOR_POINT_OF_INTEREST_STATES),
-                    1,
-                    1
             )
     );
 
