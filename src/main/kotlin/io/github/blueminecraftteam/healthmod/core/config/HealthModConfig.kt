bec4af11f6d338c7a857f4ca737cfc230dacbde1
AG6 <bean@bean.bean>
2021-01-01 18:03:00 +0000

    forgot to commit config

---
+++ src/main/kotlin/io/github/blueminecraftteam/healthmod/core/config/HealthModConfig.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020 Blue Minecraft Team.
+ *
+ * This file is part of HealthMod.
+ *
+ * HealthMod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HealthMod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with HealthMod.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.github.blueminecraftteam.healthmod.core.config
+
+import io.github.blueminecraftteam.healthmod.core.HealthMod
+import net.minecraftforge.common.ForgeConfigSpec
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber
+import org.apache.commons.lang3.tuple.Pair
+
+@EventBusSubscriber(modid = HealthMod.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+object HealthModConfig {
+    private val specPair: Pair<ServerConfig, ForgeConfigSpec> =
+            ForgeConfigSpec.Builder().configure { builder: ForgeConfigSpec.Builder -> ServerConfig(builder) }
+    val SERVER_CONFIG: ServerConfig = specPair.left
+    val SERVER_SPEC: ForgeConfigSpec = specPair.right
+}
