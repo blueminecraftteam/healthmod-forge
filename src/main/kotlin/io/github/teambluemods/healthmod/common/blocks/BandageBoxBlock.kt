0e0a7375db447e21334cf5015ec2ee26750947b0
Bioastroiner <naderi81eco@gmail.com>
2021-01-25 14:14:59 +0330

    done some adding openGui logic to BandageBox

--- src/main/kotlin/io/github/teambluemods/healthmod/common/blocks/BandageBoxBlock.kt
+++ src/main/kotlin/io/github/teambluemods/healthmod/common/blocks/BandageBoxBlock.kt
@@ -20,19 +20,9 @@
 package io.github.teambluemods.healthmod.common.blocks
 
 import io.github.blueminecraftteam.healthmod.core.registries.TileEntityTypeRegistries
-import io.github.teambluemods.healthmod.common.tileentities.BandageBoxTileEntity
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
-import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.entity.player.ServerPlayerEntity
-import net.minecraft.stats.Stats
-import net.minecraft.util.ActionResultType
-import net.minecraft.util.Hand
-import net.minecraft.util.math.BlockPos
-import net.minecraft.util.math.BlockRayTraceResult
 import net.minecraft.world.IBlockReader
-import net.minecraft.world.World
-import net.minecraftforge.fml.network.NetworkHooks
 
 class BandageBoxBlock(properties: Properties) : Block(properties) {
     // TODO: Model
